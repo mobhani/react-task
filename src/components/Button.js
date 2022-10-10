154a2f332e0e65d42bf4c8b4be0562be0f2682f7
mobhani <zainmobhani_b@outlook.com>
2022-10-09 17:04:20 -0700

    Button, Header complete

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,13 @@
-function Button({color, text}) {
+function Button({color, text, onClick}) {
+
+
   return (
-    <button className='btn' style={{backgroundColor: color}} >{text}</button>
+    <button 
+    className='btn' 
+    style={{backgroundColor: color}}
+    onClick={onClick}>
+        {text}
+    </button>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import Button from "./Button"
 
 const Header = ({title}) => {
+
+    const onClick = () => {
+        console.log("clock")
+    }
+
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color='black' text='Add'/>
+        <Button color='black' text='Add' onClick={onClick}/>
         
     </header>
   )
